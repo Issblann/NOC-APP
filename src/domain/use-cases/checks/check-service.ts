211e9ecc20ed4501a5e97c4f1050d979b2e98e1a
Issblann <anaisabelpp@outlook.com>
2024-04-13 12:09:47 -0500

    apply repository pattern, file system datasource

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -15,10 +19,17 @@ export class CheckService implements CheckServiceUseCase {
       const req = await fetch(url);
       if (!req.ok) throw new Error(`Error on check service ${url}`);
 
+      const log = new LogEntity(LogSeverityLevel.low, `Service ${url} working`);
+      this.logRepository.saveLog(log);
       this.successCallback();
       return true;
     } catch (error) {
-      this.errorCallback(`${error}`);
+      const errorMessage = `${error}`;
+      const log = new LogEntity(LogSeverityLevel.high, errorMessage);
+      this.logRepository.saveLog(log);
+
+      this.errorCallback(errorMessage);
+
       return false;
     }
   }
