65a12280dfd2044787d892eb7061535e27462f37
Issblann <anaisabelpp@outlook.com>
2024-04-17 11:45:05 -0500

    add conection posgresql, datasource and prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import { envs } from "./config/plugins/envs.plugin";
 import { LogModel, MongoDataBase } from "./data/mongo";
 import { Server } from "./presentation/server";
@@ -11,6 +12,21 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
   });
+
+  // const prisma = new PrismaClient();
+  // const newLog = await prisma.logModel.create({
+  //   data: {
+  //     level: "LOW",
+  //     message: "Test message",
+  //     origin: "App.ts",
+  //   },
+  // });
+  // const logs = await prisma.logModel.findMany({
+  //   where: {
+  //     level: "LOW",
+  //   },
+  // });
+  // console.log(logs);
   Server.start();
   // const logs = await LogModel.find();
   // console.log(logs);
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -4,11 +4,15 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDataSource } from "../infrastructure/datasources/mongo-log.datasource";
+import { PostresLogDataSource } from "../infrastructure/datasources/postgres-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
 import { EmailService } from "./email/email.service";
 
-const logRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const logRepository = new LogRepositoryImpl(
+  // new MongoLogDataSource()
+  new PostresLogDataSource()
+);
 const emailService = new EmailService();
 export class Server {
   public static async start() {
@@ -20,17 +24,17 @@ export class Server {
     // ]);
     // emailService.sendEmailWithFileSystemLogs(["anaisabelpp@outlook.com"]);
 
-    // CronService.createJob("*/5 * * * * *", () => {
-    //   const url = "https://google.com";
-    //   new CheckService(
-    //     logRepository,
-    //     () => console.log(`Service is up ${url}`),
-    //     (error) => console.log(`Service is down ${error}`)
-    //   ).execute(url);
-    // });
+    CronService.createJob("*/5 * * * * *", () => {
+      const url = "https://googasdle.com";
+      new CheckService(
+        logRepository,
+        () => console.log(`Service is up ${url}`),
+        (error) => console.log(`Service is down ${error}`)
+      ).execute(url);
+    });
 
-    const logs = await logRepository.getLogs(LogSeverityLevel.low);
-    console.log(logs);
+    // const logs = await logRepository.getLogs(LogSeverityLevel.low);
+    // console.log(logs);
   }
 }
 
